3d6fee2fae6d17c4ba3d8f84c4c1fa2abaf745d9
Nataset Tanabodee <nataset.t@ku.th>
2021-10-30 22:42:35 +0700

    edit uploadController

--- app/Http/Controllers/Api/UserOrderController.php
+++ app/Http/Controllers/Api/UserOrderController.php
@@ -124,7 +124,6 @@ class UserOrderController extends Controller
         }
 
         $upload = new UploadController();
-        return $request;
         $upload_res = $upload->upload($request);
         $order_id = $id;
         $order = UserOrder::findOrFail($order_id);
